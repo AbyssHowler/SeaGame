4bec55c3c5b1d246b8c9ad3c5084789c541e91b0
AbyssHowler <minecraftdark37@gmail.com>
2025-05-18 03:31:23 +0900

    업로드

---
+++ SeaGame/Source/seaGame/seaGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "seaGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AseaGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AseaGameGameMode();
+};
+
+
+
